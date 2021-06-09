fe2313be74cd4f4d4963bf4285c60a1532cf1f75
vl4deee11 <44677024+vl4deee11@users.noreply.github.com>
2021-06-09 10:37:49 +0300

    Update postgres_exporter.go
    Signed-off-by: vl4deee11 <boi4enkovlad@yandex>
    Signed-off-by: vl4deee11 <vl4deee11@gmail.com>
    Signed-off-by: vl4deee11 <boi4enkovlad@yandex.ru>

--- cmd/postgres_exporter/postgres_exporter.go
+++ cmd/postgres_exporter/postgres_exporter.go
@@ -533,16 +533,29 @@ func parseUserQueries(content []byte, pgVersion semver.Version) (map[string]inte
 	for metric, specs := range userQueries {
 		level.Debug(logger).Log("msg", "New user metric namespace from YAML metric", "metric", metric, "cache_seconds", specs.CacheSeconds)
 		newQueryOverrides[metric] = specs.Query
-		for i := len(specs.BreakingChanges) - 1; i >= 0; i-- {
-			if err := specs.BreakingChanges[i].ParseVerTolerant(); err != nil {
+		columnT := make(map[string]string, 0)
+		for i := range specs.BreakingChanges {
+			if err := specs.BreakingChanges[i].parseVerTolerant(); err != nil {
 				return nil, nil, err
 			}
 
 			if pgVersion.GE(specs.BreakingChanges[i].ver) {
-				newQueryOverrides[metric] = specs.BreakingChanges[i].FixColumns(specs.Query)
-				break
+				for t := range specs.BreakingChanges[i].Columns {
+					columnT[t] = specs.BreakingChanges[i].Columns[t]
+				}
 			}
 		}
+
+		// nolint: golint
+		// 2 because old - new
+		oldnew := make([]string, 0, 2*len(columnT))
+		for t := range columnT {
+			oldnew = append(oldnew, t, columnT[t])
+		}
+
+		r := strings.NewReplacer(oldnew...)
+
+		newQueryOverrides[metric] = r.Replace(newQueryOverrides[metric])
 
 		metricMap, ok := metricMaps[metric]
 		if !ok {
