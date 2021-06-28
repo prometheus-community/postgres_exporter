1b9e76d58385a9558a7bed296bba82a184efca3e
Michael Todorovic <michael.todorovic@outlook.com>
2021-06-28 16:40:05 +0200

    fix: handle pg_replication_slots on replicas and cascading replicas
    Signed-off-by: Michael Todorovic <michael.todorovic@outlook.com>

--- cmd/postgres_exporter/postgres_exporter.go
+++ cmd/postgres_exporter/postgres_exporter.go
@@ -432,14 +432,16 @@ var queryOverrides = map[string][]OverrideQuery{
 		{
 			semver.MustParseRange(">=9.4.0 <10.0.0"),
 			`
-			SELECT slot_name, database, active, pg_xlog_location_diff(pg_current_xlog_location(), restart_lsn)
+			SELECT slot_name, database, active,
+				(case pg_is_in_recovery() when 't' then pg_xlog_location_diff(pg_current_xlog_location(), restart_lsn) else pg_xlog_location_diff(pg_last_xlog_receive_location(), restart_lsn) as pg_xlog_location_diff
 			FROM pg_replication_slots
 			`,
 		},
 		{
 			semver.MustParseRange(">=10.0.0"),
 			`
-			SELECT slot_name, database, active, pg_wal_lsn_diff(pg_current_wal_lsn(), restart_lsn)
+			SELECT slot_name, database, active,
+				(case pg_is_in_recovery() when 't' then pg_wal_lsn_diff(pg_last_wal_receive_lsn(), restart_lsn) else pg_wal_lsn_diff(pg_current_wal_lsn(), restart_lsn) end) as pg_wal_lsn_diff
 			FROM pg_replication_slots
 			`,
 		},
