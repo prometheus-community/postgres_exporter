704dabf908195d4583d28b522e717ffc986ada8a
Felix Yuan <felix.yuan@reddit.com>
2023-06-28 11:15:12 -0700

    Total relation size collector and test
    Signed-off-by: Felix Yuan <felix.yuan@reddit.com>

---
+++ collector/pg_total_relation_size.go
@@ -0,0 +1,98 @@
+// Copyright 2023 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package collector
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/go-kit/log"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+const totalRelationSizeSubsystem = "total_relation_size"
+
+func init() {
+	registerCollector(totalRelationSizeSubsystem, defaultDisabled, NewPGTotalRelationSizeCollector)
+}
+
+type PGTotalRelationSizeCollector struct {
+	log log.Logger
+}
+
+func NewPGTotalRelationSizeCollector(config collectorConfig) (Collector, error) {
+	return &PGTotalRelationSizeCollector{log: config.logger}, nil
+}
+
+var (
+	totalRelationSizeBytes = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, totalRelationSizeSubsystem, "bytes"),
+		"total disk space usage for the specified table and associated indexes",
+		[]string{"schemaname", "relname"},
+		prometheus.Labels{},
+	)
+
+	totalRelationSizeQuery = `
+		SELECT
+			relnamespace::regnamespace as schemaname,
+			relname as relname,
+			pg_total_relation_size(oid) bytes
+		FROM pg_class
+		WHERE relkind = 'r';
+	`
+)
+
+func (PGTotalRelationSizeCollector) Update(ctx context.Context, instance *instance, ch chan<- prometheus.Metric) error {
+	db := instance.getDB()
+	rows, err := db.QueryContext(ctx,
+		totalRelationSizeQuery)
+
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var schemaname, relname sql.NullString
+		var bytes sql.NullFloat64
+
+		if err := rows.Scan(&schemaname, &relname, &bytes); err != nil {
+			return err
+		}
+		schemanameLabel := "unknown"
+		if schemaname.Valid {
+			schemanameLabel = schemaname.String
+		}
+		relnameLabel := "unknown"
+		if relname.Valid {
+			relnameLabel = relname.String
+		}
+		labels := []string{schemanameLabel, relnameLabel}
+
+		bytesMetric := 0.0
+		if bytes.Valid {
+			bytesMetric = bytes.Float64
+		}
+		ch <- prometheus.MustNewConstMetric(
+			totalRelationSizeBytes,
+			prometheus.GaugeValue,
+			bytesMetric,
+			labels...,
+		)
+	}
+	if err := rows.Err(); err != nil {
+		return err
+	}
+	return nil
+}
