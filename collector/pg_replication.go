0b6d9860abb2340412a15be1fc2b4d25e154b8c3
Vladimir Luksha <waldemarluksha@gmail.com>
2023-09-08 23:20:19 +0300

    fix pg_replication_lag_seconds (#895)
    Signed-off-by: Vladimir Luksha <waldemarluksha@gmail.com>
    Co-authored-by: Vladimir Luksha <luksha@limcore.io>

--- collector/pg_replication.go
+++ collector/pg_replication.go
@@ -55,6 +55,7 @@ var (
 	pgReplicationQuery = `SELECT
 	CASE
 		WHEN NOT pg_is_in_recovery() THEN 0
+                WHEN pg_last_wal_receive_lsn () = pg_last_wal_replay_lsn () THEN 0
 		ELSE GREATEST (0, EXTRACT(EPOCH FROM (now() - pg_last_xact_replay_timestamp())))
 	END AS lag,
 	CASE
