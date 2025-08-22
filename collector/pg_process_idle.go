3fc80a7a2c30026ab27ee204a8dc44a74a09ec7d
Kaarel Moppel <kaarel.moppel@gmail.com>
2025-08-22 23:50:24 +0300

    Exclude the metrics fetching session's data from pg_stat_activity
    To reduce the observer effect, filter out pg_stat_activity rows of the
    postgres_exporter session from all SA queries, based on pid / procpid.
    
    A bit annoying to see idling DBs showing pg_stat_activity_count "active"
    count of 1
    
    Signed-off-by: Kaarel Moppel <kaarel.moppel@gmail.com>

--- collector/pg_process_idle.go
+++ collector/pg_process_idle.go
@@ -56,6 +56,7 @@ func (PGProcessIdleCollector) Update(ctx context.Context, instance *instance, ch
 				COUNT(*) AS process_idle_seconds_count
 				FROM pg_stat_activity
 				WHERE state ~ '^idle'
+				AND pid <> pg_backend_pid();
 				GROUP BY state, application_name
 			),
 			buckets AS (
@@ -72,6 +73,7 @@ func (PGProcessIdleCollector) Update(ctx context.Context, instance *instance, ch
 				FROM
 				pg_stat_activity,
 				UNNEST(ARRAY[1, 2, 5, 15, 30, 60, 90, 120, 300]) AS le
+				WHERE pid <> pg_backend_pid()
 				GROUP BY state, application_name, le
 				ORDER BY state, application_name, le
 			)
