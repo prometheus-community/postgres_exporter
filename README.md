619e34f11e4edebe310a60af0e7290b1a33ac07e
Pierre SOUCHAY <pierre.souchay@pelico.io>
2025-12-12 00:24:31 +0100

    document the new variable `PG_EXPORTER_COLLECTION_TIMEOUT` in the README.md
    Signed-off-by: Pierre SOUCHAY <pierre.souchay@pelico.io>

--- README.md
+++ README.md
@@ -255,6 +255,13 @@ The following environment variables configure the exporter:
 * `DATA_SOURCE_PASS_FILE`
   The same as above but reads the password from a file.
 
+* `PG_EXPORTER_COLLECTION_TIMEOUT`
+  Timeout duration to use when collecting the statistics, default to `1m`.
+  When the timeout is reached, the database connection will be dropped.
+  It avoids connections stacking when the database answers too slowly
+  (for instance if the database creates/drop a huge table and locks the tables)
+  and will avoid exhausting the pool of connections of the database.
+
 * `PG_EXPORTER_WEB_TELEMETRY_PATH`
   Path under which to expose metrics. Default is `/metrics`.
 
--- cmd/postgres_exporter/main.go
+++ cmd/postgres_exporter/main.go
@@ -50,7 +50,7 @@ var (
 	excludeDatabases       = kingpin.Flag("exclude-databases", "A list of databases to remove when autoDiscoverDatabases is enabled (DEPRECATED)").Default("").Envar("PG_EXPORTER_EXCLUDE_DATABASES").String()
 	includeDatabases       = kingpin.Flag("include-databases", "A list of databases to include when autoDiscoverDatabases is enabled (DEPRECATED)").Default("").Envar("PG_EXPORTER_INCLUDE_DATABASES").String()
 	metricPrefix           = kingpin.Flag("metric-prefix", "A metric prefix can be used to have non-default (not \"pg\") prefixes for each of the metrics").Default("pg").Envar("PG_EXPORTER_METRIC_PREFIX").String()
-	collectionTimeout      = kingpin.Flag("collection-timeout", "Maximum duration of collection").Default("1m").Envar("PG_EXPORTER_COLLECTION_TIMEOUT").String()
+	collectionTimeout      = kingpin.Flag("collection-timeout", "Timeout for collecting the statistics when the database is slow").Default("1m").Envar("PG_EXPORTER_COLLECTION_TIMEOUT").String()
 	logger                 = promslog.NewNopLogger()
 )
 
